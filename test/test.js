d2a8b1b76802a030802466e8563b485d19f19945
Nathan Faucett <nathanfaucett@gmail.com>
2015-02-19 18:17:57 -0600

    updates

--- test/test.js
+++ test/test.js
@@ -3,37 +3,57 @@ var assert = require("assert");
 
 describe("PromisePolyfill", function() {
     it("should polyfill promise if not present in global", function(done) {
-        var PromisePolyfill,
-            sync = true;
+        var Promise = global.Promise,
+            PromisePolyfill;
 
         global.Promise = undefined;
         PromisePolyfill = require("../src/index");
 
-        new PromisePolyfill(function resolver(resolve, reject) {
-            if (Math.random() <= 0.5 ? true : false) {
-                resolve(true);
-            } else {
-                reject(false);
-            }
-        }).then(
-            function success(value) {
-                sync = false;
-                assert.equal(value, true);
-                return "asdf";
-            },
-            function error(value) {
-                sync = false;
-                assert.equal(value, false);
-                done();
-            }
-        ).then(
-            function success(value) {
-                sync = false;
-                assert.equal(value, "asdf");
-                done();
-            }
-        );
+        function run(Promise, value, callback) {
+            var sync = true;
 
-        assert.equal(sync, true);
+            new Promise(function resolver(resolve, reject) {
+                if (value) {
+                    resolve(true);
+                } else {
+                    reject(false);
+                }
+            }).then(
+                function success(value) {
+                    sync = false;
+                    assert.equal(value, true);
+                    return "asdf";
+                },
+                function error(value) {
+                    sync = false;
+                    assert.equal(value, false);
+                    callback();
+                    return value;
+                }
+            ).then(
+                function success(value) {
+                    sync = false;
+                    assert.equal(value, "asdf");
+                    callback();
+                }
+            );
+            assert.equal(sync, true);
+        }
+
+        run(Promise, true, function(err) {
+            if (err) {
+                done(err);
+            } else {
+                run(Promise, false, function() {
+                    run(PromisePolyfill, true, function(err) {
+                        if (err) {
+                            done(err);
+                        } else {
+                            run(PromisePolyfill, false, done);
+                        }
+                    });
+                });
+            }
+        });
     });
 });
